1656ed6ad430a8f981415d07c68e3e7f782ea8bd
unknown <siammdarafathislam@gmail.com>
2023-10-22 23:02:33 +0600

    faculty module updated

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { FacultyController } from './faculty.controller';
 import { FacultyValidation } from './faculty.validations';
@@ -11,8 +13,19 @@ router.get('/:id', FacultyController.getByIdFromDB);
 
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(FacultyValidation.create),
   FacultyController.insertIntoDB
+);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.updateIntoDB
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  FacultyController.deleteFromDB
 );
 
 export const facultyRoutes = router;
